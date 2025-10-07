a8e23ed9f0d762fab006bf54e31bb30595150b24
fatemaalfardan59 <fatemaalfardan5@gmail.com>
2025-10-07 15:29:34 +0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,3 @@
+document . getElementById (" contractForm "). onsubmit = function (){ alert (" Your inquiry has neen submitted .");};
+ ocument . getElemantByTagName (" tr ")[1]. onclick = fuction ){ alert (" monday class at 10-11");};
+ document . getElementsByTagName (" tr ")[2]. onclick = function (){ alert (-");};
